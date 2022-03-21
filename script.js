f37436f785a36d4f5e9f5c85c9c9c5796252ee14
jonniedd <jonathandom01@gmail.com>
2022-03-20 22:32:52 -0500

    added category section to new-home; added styling and js to it. Needs fixing

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 /** First Slider FIX/INCOMPLETE */ 
 $('.carousel').carousel();
+
+/** Second Slider FIX */
+
+$("slider-two")
+.not(".slick-initialized")
+.slick({
+    prevArrow:".side-slider.two .prev",
+    nextArrow:".site-slider.two .next",
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplaySpeed: 3000
+});
