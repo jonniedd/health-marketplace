56a12fc83cd9565a2f26d555d5904cce18708d82
jonniedd <jonathandom01@gmail.com>
2022-03-20 21:10:57 -0500

    updated javascript lines

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
 /** First Slider FIX/INCOMPLETE */ 
-$(".slider-one").slick
-.not(".slick-initialized")
-.slick({
-    autoplay: true,
-    autoplaySpeed: 3000,
-    dots: true
-});
+$('.carousel').carousel();
