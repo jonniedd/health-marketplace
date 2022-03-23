08bfe49b2bdd9c08d418d5192eeb30535603b2be
jonniedd <jonathandom01@gmail.com>
2022-03-23 16:36:15 -0500

    updated carousel element

--- script.js
+++ script.js
@@ -1,14 +1,2 @@
-/** First Slider FIX/INCOMPLETE */ 
+/** First Slider INCOMPLETE */ 
 $('.carousel').carousel();
-
-/** Second Slider FIX */
-
-$("slider-two")
-.not(".slick-initialized")
-.slick({
-    prevArrow:".side-slider.two .prev",
-    nextArrow:".site-slider.two .next",
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    autoplaySpeed: 3000
-});
